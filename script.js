94cc01f08e5d9ea4ecfffdb15fa9025627d80aca
NewtonKamau <kamaunewton78@gmail.com>
2021-01-06 00:54:27 +0300

    using asyn to get the jokes

--- script.js
+++ script.js
@@ -3,15 +3,30 @@ const jokeBtn = document.getElementById("jokeBtn");
 
 generateJoke();
 
-function generateJoke(){
+jokeBtn.addEventListener("click",generateJoke);
+
+//using async and wait
+async function generateJoke(){
     const config = {
         headers:{
             "Accept":"application/json"
         }
     }
-    fetch("https://icanhazdadjoke.com",config)
-    .then((res)=>res.json())
-    .then((data) => {
-        jokeEl.innerHTML = data.joke;
-    });
+   const response = await fetch("https://icanhazdadjoke.com",config)
+   
+   const data = await response.json();
+   jokeEl.innerHTML = data.joke;
 };
+//using .then
+// function generateJoke(){
+//     const config = {
+//         headers:{
+//             "Accept":"application/json"
+//         }
+//     }
+//     fetch("https://icanhazdadjoke.com",config)
+//     .then((res)=>res.json())
+//     .then((data) => {
+//         jokeEl.innerHTML = data.joke;
+//     });
+// };
